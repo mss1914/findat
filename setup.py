64c70058956f0066b60d120c2302cdabc9891d2f
MStrohmann <mstrohmann@gmail.com>
2016-03-02 16:47:18 -0600

    Added files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='findat',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/mss1914/findat',
+      author='Michael Strohmann',
+      author_email='mstrohmann@csforecasts.com',
+      license='MIT',
+      packages=['findat'],
+      zip_safe=False)
