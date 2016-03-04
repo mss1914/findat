b101bf509f943324722d9518ecde51f6dd9350af
MStrohmann <mstrohmann@gmail.com>
2016-03-04 12:31:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='findat',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Financial Data Retrieval and Analysis',
       url='https://github.com/mss1914/findat',
       author='Michael Strohmann',
       author_email='mstrohmann@csforecasts.com',
-      license='MIT',
+      license='',
       packages=['findat'],
       zip_safe=False)
