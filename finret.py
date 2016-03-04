d96ab8df85c526dee789f5d7f3eb951fe98a8e10
MStrohmann <mstrohmann@gmail.com>
2016-03-04 12:18:08 -0600

    Update and rename get_data.py to finret.py

--- finret.py
+++ finret.py
@@ -1,12 +1,20 @@
+import numpy as np
+import scipy as sp
+import pandas as p
 import urllib
 import json
 import socket
 import os
 import time
+import sys
 from datetime import *
-import pandas as p
-import numpy as np
-import scipy as sp
+from scipy.stats import norm
+from scipy.stats import multivariate_normal
+from scipy.stats import chi2
+from scipy.stats import laplace
+from scipy.stats import percentileofscore
+from random import randint
+
 
 class fred_api:
 
@@ -31,23 +39,14 @@ class fred_api:
                             + x + '&api_key=' + self.FRED_API_Key
                             + '&file_type=' + self.file_type)
                            
-                ##Get the Data
-                failloop = 1   
-                while True:
-                    try:
-                        req = urllib.request.Request(self.url)
-                        url_open = urllib.request.urlopen(req)
-                        data = url_open.read().decode('utf-8',
-                                                      errors='ignore')
-                        break
-                    except Exception as e:
-                        print('Unable to connect to FRED; trying again.')
-                        time.sleep(10)
-                        failloop += 1
-                        if failloop > 3:
-                            print('Unable to connect to FRED; Check '
-                                  + 'your API key')
-                            sys.exit()
+                ##Get the Data   
+                try:
+                    req = urllib.request.Request(self.url)
+                    url_open = urllib.request.urlopen(req)
+                    data = url_open.read().decode('utf-8',errors='ignore')
+                except Exception as e:
+                    print(str(e) + '; check your Series ID')
+                    sys.exit()
 
                 ##Put Data into Pandas DataFrame
                 data = json.loads(data)
@@ -76,7 +75,7 @@ class fred_api:
     ##Get Series Observations##
     def observations(self):
 
-        root_url = ('https://api.stlouisfed.org/fred/series/
+        root_url = ('https://api.stlouisfed.org/fred/series/'
                     + 'observations?series_id=')
 
         if type(self.series_id) == list:
@@ -92,22 +91,13 @@ class fred_api:
                                 + '&file_type=' + self.file_type)
                                
                     ##Get the Data
-                    failloop = 1   
-                    while True:
-                        try:
-                            req = urllib.request.Request(self.url)
-                            url_open = urllib.request.urlopen(req)
-                            data = url_open.read().decode('utf-8',
-                                                          errors='ignore')
-                            break
-                        except Exception as e:
-                            print('Unable to connect to FRED; trying again.')
-                            time.sleep(10)
-                            failloop += 1
-                            if failloop > 3:
-                                print('Unable to connect to FRED; Check '
-                                      + 'your API key')
-                                sys.exit()
+                    try:
+                        req = urllib.request.Request(self.url)
+                        url_open = urllib.request.urlopen(req)
+                        data = url_open.read().decode('utf-8',errors='ignore')
+                    except Exception as e:
+                        print(e)
+                        sys.exit()
 
                     ##Put Data into Pandas DataFrame
                     data = json.loads(data)
@@ -141,22 +131,13 @@ class fred_api:
                                 + '&file_type=' + self.file_type)
                                
                     ##Get the Data
-                    failloop = 1   
-                    while True:
-                        try:
-                            req = urllib.request.Request(self.url)
-                            url_open = urllib.request.urlopen(req)
-                            data = url_open.read().decode('utf-8',
-                                                          errors='ignore')
-                            break
-                        except Exception as e:
-                            print('Unable to connect to FRED; trying again.')
-                            time.sleep(10)
-                            failloop += 1
-                            if failloop > 3:
-                                print('Unable to connect to FRED; Check '
-                                      + 'your API key')
-                                sys.exit()
+                    try:
+                        req = urllib.request.Request(self.url)
+                        url_open = urllib.request.urlopen(req)
+                        data = url_open.read().decode('utf-8',errors='ignore')
+                    except Exception as e:
+                        print(e)
+                        sys.exit()
 
                     ##Put Data into Pandas DataFrame
                     data = json.loads(data)
@@ -191,22 +172,13 @@ class fred_api:
                                 + '&file_type=' + self.file_type)
                                
                     ##Get the Data
-                    failloop = 1   
-                    while True:
-                        try:
-                            req = urllib.request.Request(self.url)
-                            url_open = urllib.request.urlopen(req)
-                            data = url_open.read().decode('utf-8',
-                                                          errors='ignore')
-                            break
-                        except Exception as e:
-                            print('Unable to connect to FRED; trying again.')
-                            time.sleep(10)
-                            failloop += 1
-                            if failloop > 3:
-                                print('Unable to connect to FRED; Check '
-                                      + 'your API key')
-                                sys.exit()
+                    try:
+                        req = urllib.request.Request(self.url)
+                        url_open = urllib.request.urlopen(req)
+                        data = url_open.read().decode('utf-8',errors='ignore')
+                    except Exception as e:
+                        print(e)
+                        sys.exit()
 
                     ##Put Data into Pandas DataFrame
                     data = json.loads(data)
@@ -241,22 +213,13 @@ class fred_api:
                                 + '&file_type=' + self.file_type)
                                
                     ##Get the Data
-                    failloop = 1   
-                    while True:
-                        try:
-                            req = urllib.request.Request(self.url)
-                            url_open = urllib.request.urlopen(req)
-                            data = url_open.read().decode('utf-8',
-                                                          errors='ignore')
-                            break
-                        except Exception as e:
-                            print('Unable to connect to FRED; trying again.')
-                            time.sleep(10)
-                            failloop += 1
-                            if failloop > 3:
-                                print('Unable to connect to FRED; Check '
-                                      + 'your API key')
-                                sys.exit()
+                    try:
+                        req = urllib.request.Request(self.url)
+                        url_open = urllib.request.urlopen(req)
+                        data = url_open.read().decode('utf-8',errors='ignore')
+                    except Exception as e:
+                        print(e)
+                        sys.exit()
 
                     ##Put Data into Pandas DataFrame
                     data = json.loads(data)
@@ -290,22 +253,13 @@ class fred_api:
                                 + '&file_type=' + self.file_type)
                                
                     ##Get the Data
-                    failloop = 1   
-                    while True:
-                        try:
-                            req = urllib.request.Request(self.url)
-                            url_open = urllib.request.urlopen(req)
-                            data = url_open.read().decode('utf-8',
-                                                          errors='ignore')
-                            break
-                        except Exception as e:
-                            print('Unable to connect to FRED; trying again.')
-                            time.sleep(10)
-                            failloop += 1
-                            if failloop > 3:
-                                print('Unable to connect to FRED; Check '
-                                      + 'your API key')
-                                sys.exit()
+                    try:
+                        req = urllib.request.Request(self.url)
+                        url_open = urllib.request.urlopen(req)
+                        data = url_open.read().decode('utf-8',errors='ignore')
+                    except Exception as e:
+                        print(e)
+                        sys.exit()
 
                     ##Put Data into Pandas DataFrame
                     data = json.loads(data)
@@ -338,21 +292,13 @@ class fred_api:
                         + '&file_type=' + self.file_type)
                        
             ##Get the Data
-            failloop = 1   
-            while True:
-                try:
-                    req = urllib.request.Request(self.url)
-                    url_open = urllib.request.urlopen(req)
-                    data = url_open.read().decode('utf-8', errors='ignore')
-                    break
-                except Exception as e:
-                    print('Unable to connect to FRED; trying again.')
-                    time.sleep(10)
-                    failloop += 1
-                    if failloop > 3:
-                        print('Unable to connect to FRED; Check '
-                              + 'your API key')
-                        sys.exit()
+            try:
+                req = urllib.request.Request(self.url)
+                url_open = urllib.request.urlopen(req)
+                data = url_open.read().decode('utf-8', errors='ignore')
+            except Exception as e:
+                print(e)
+                sys.exit()
 
             ##Put Data into Pandas DataFrame
             data = json.loads(data)
@@ -369,35 +315,35 @@ class fred_api:
 def shiller_cape():
     ##Get Shiller's CAPE Data
     location = 'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
-    df = p.read_excel(location,
-                           sheetname='Data',
-                           header=7)
+    
+    try:
+        df = p.read_excel(location,sheetname='Data',header=7)
+    except Exception as e:
+        print(e)
+        sys.exit()
+            
     df = df[np.isfinite(df.CAPE)]
     df = df.dropna(axis=1, how='all')
     index = p.date_range('1881-1-1', periods=df.CAPE.shape[0], freq='M')
+    df = df.drop(['Date','Fraction'], axis=1)
+    df_columns = ['NPrice','NDiv','NEarnings','CPI','GS10_Rate',
+                  'RPrice', 'RDiv', 'REarnings', 'CAPE']
+    df.columns = df_columns
     df.index = index
     
     return df
 
 def yahoo_sp(stock_symbol):
     ##Stock price information from yahoo finance.
-    failloop = 1
-    while True:
-        try:
-            req = urllib.request.Request('http://finance.yahoo.com/q/hp?s='
-                                         + stock_symbol
-                                         + '+Historical+Prices')
-            yahoo_url = urllib.request.urlopen(req)
-            yahoo_url_text = yahoo_url.read().decode('utf-8', errors='ignore')
-            break
-        except Exception as e:
-            print('Unable to connect to FRED; trying again.')
-            time.sleep(10)
-            failloop += 1
-            if failloop > 3:
-                print('Unable to connect to FRED; Check '
-                      + 'your API key')
-                sys.exit()
+    try:
+        req = urllib.request.Request('http://finance.yahoo.com/q/hp?s='
+                                     + stock_symbol
+                                     + '+Historical+Prices')
+        yahoo_url = urllib.request.urlopen(req)
+        yahoo_url_text = yahoo_url.read().decode('utf-8', errors='ignore')
+    except Exception as e:
+        print(e)
+        sys.exit()
 
     if yahoo_url_text.find('Download to Spreadsheet') != -1:
         ##Parse the html text to get the csv url
@@ -409,10 +355,17 @@ def yahoo_sp(stock_symbol):
         url_address = yahoo_url_text2[:url_end]
 
         ##Create a data frame from csv
-        stock_df = pandas.DataFrame.from_csv(url_address, sep=',')
+        stock_df = p.DataFrame.from_csv(url_address, sep=',')
 
         ##Add a date column
         stock_df['close_date'] = stock_df.index
 
         ##Add a symbol column
         stock_df['symbol'] = stock_symbol
+
+        return stock_df
+
+    else:
+
+        stock_df = p.DataFrame()
+        return stock_df
